0c3e921bbfab74cd57ed74317b4a21171a019e3b
lakmalmabm <lakmalmabm@gmail.com>
2024-09-08 21:37:23 +0530

    register page ffix

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,7 +3,7 @@ import SignIn from "@/components/supaauth/signin"
 export default function Registerpage() {
     return(
 
-        <div className="w-full h-full flex p-5 md:5-0 flex-col items-center justify-center min-h-screen">
+        <div className="w-full h-full flex p-5 md:p-0 flex-col items-center justify-center min-h-screen">
                     <SignIn/>
         </div>
 
